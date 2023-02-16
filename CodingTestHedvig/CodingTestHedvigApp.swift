719a02795cd83c503bb2bd7340d6a238128a3a6a
davidmansourian <mada20sv@student.ju.se>
2023-02-16 10:35:38 +0100

    Initial Commit

---
+++ CodingTestHedvig/CodingTestHedvigApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTestHedvigApp.swift
+//  CodingTestHedvig
+//
+//  Created by David on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct CodingTestHedvigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
