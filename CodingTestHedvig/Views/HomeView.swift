84310ebcc30ab11ec663f405690cbae8762595f1
davidmansourian <mada20sv@student.ju.se>
2023-02-16 10:42:31 +0100

    Initial commit

---
+++ CodingTestHedvig/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  CodingTestHedvig
+//
+//  Created by David on 2023-02-16.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
